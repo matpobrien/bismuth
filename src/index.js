c9eb47af03ecced42a9e29d9577826570eab49e9
Matthew Paul O'Brien <obrien.matthew07@gmail.com>
2020-09-02 19:30:16 +0200

    add new transitions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// const anime = require("animejs");
+
 const colors = document.querySelectorAll("div");
 const buttons = document.querySelectorAll("h1");
 const title = document.getElementById("title");
@@ -11,7 +13,95 @@ title.addEventListener("click", () => {
   });
 });
 
+document
+  .querySelector(".invisilayer")
+  .addEventListener("mousemove", (event) => {
+    const pageX = event.clientX;
+    const pageY = event.clientY;
+    console.log(pageY);
+    const topSides = document.querySelectorAll(".top");
+    const bottomSides = document.querySelectorAll(".bottom");
+    const leftSides = document.querySelectorAll(".left");
+    const rightSides = document.querySelectorAll(".right");
+    if (pageX > window.innerWidth / 2) {
+      leftSides.forEach((layer) => {
+        layer.classList.remove("translate-left");
+        layer.classList.add("translate-right");
+      });
+      rightSides.forEach((layer) => {
+        layer.classList.remove("translate-left");
+        layer.classList.add("translate-right");
+      });
+    } else {
+      leftSides.forEach((layer) => {
+        layer.classList.add("translate-left");
+        layer.classList.remove("translate-right");
+      });
+      rightSides.forEach((layer) => {
+        layer.classList.add("translate-left");
+        layer.classList.remove("translate-right");
+      });
+    }
+    if (pageY > window.innerHeight / 2) {
+      topSides.forEach((layer) => {
+        layer.classList.add("translate-up");
+        layer.classList.remove("translate-down");
+      });
+      bottomSides.forEach((layer) => {
+        layer.classList.add("translate-up");
+        layer.classList.remove("translate-down");
+      });
+    } else {
+      topSides.forEach((layer) => {
+        layer.classList.remove("translate-up");
+        layer.classList.add("translate-down");
+      });
+      bottomSides.forEach((layer) => {
+        layer.classList.remove("translate-up");
+        layer.classList.add("translate-down");
+      });
+    }
+    // console.log(screenLeft);
+  });
+
+const removeSlide = () => {
+  const leftSides = document.querySelectorAll(".left");
+  const rightSides = document.querySelectorAll(".right");
+  const topSides = document.querySelectorAll(".top");
+  const bottomSides = document.querySelectorAll(".bottom");
+  leftSides.forEach((layer) => {
+    layer.classList.remove("translate-left");
+    layer.classList.remove("translate-right");
+  });
+  rightSides.forEach((layer) => {
+    layer.classList.remove("translate-left");
+    layer.classList.remove("translate-right");
+  });
+  topSides.forEach((layer) => {
+    layer.classList.remove("translate-up");
+    layer.classList.remove("translate-down");
+  });
+  bottomSides.forEach((layer) => {
+    layer.classList.remove("translate-up");
+    layer.classList.remove("translate-down");
+  });
+};
+
+document.getElementById("remove").addEventListener("click", () => {
+  document.querySelector(".invisilayer").classList.add("hidden");
+  document.getElementById("remove").classList.add("hidden");
+  document.getElementById("enable").classList.remove("hidden");
+  removeSlide();
+});
+
+document.getElementById("enable").addEventListener("click", () => {
+  document.querySelector(".invisilayer").classList.toggle("hidden");
+  document.getElementById("remove").classList.remove("hidden");
+  document.getElementById("enable").classList.add("hidden");
+});
+
 const openMusic = () => {
+  removeSlide();
   document.querySelector(".light-green").classList.toggle("open-music");
   document.querySelector(".green").classList.toggle("open-music");
   document.querySelector(".blue-green").classList.toggle("open-music");
@@ -22,6 +112,7 @@ document.querySelector(".music").addEventListener("click", () => {
 });
 
 const openFilm = () => {
+  removeSlide();
   document.querySelector(".blue-green").classList.toggle("open-film");
   document.querySelector(".teal").classList.toggle("open-film");
   document.querySelector(".blue").classList.toggle("open-film");
@@ -35,12 +126,16 @@ document.querySelector(".film").addEventListener("click", () => {
 });
 
 const openWriting = () => {
-  openFilm();
-  document.querySelector(".writing").addEventListener("click", () => {
-    document.querySelector(".purple").classList.toggle("open-writing");
-    document.querySelector(".dark-red").classList.toggle("open-writing");
-    document.querySelector(".red").classList.toggle("open-writing");
-    document.querySelector(".magenta").classList.toggle("open-writing");
-    document.querySelector(".fuchsia").classList.toggle("open-writing");
-  });
+  removeSlide();
+  document.querySelector(".purple").classList.toggle("open-writing");
+  document.querySelector(".dark-red").classList.toggle("open-writing");
+  document.querySelector(".red").classList.toggle("open-writing");
+  document.querySelector(".magenta").classList.toggle("open-writing");
+  document.querySelector(".fuchsia").classList.toggle("open-writing");
 };
+
+document.querySelector(".writing").addEventListener("click", () => {
+  openMusic();
+  openFilm();
+  openWriting();
+});
